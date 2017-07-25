bfd97f7654740174ed4412f8cd2b34b00444645c
Minghua Sun <clairesunstudio@gmail.com>
2017-07-25 11:14:08 -0400

    hook up action creator in PostsIndex component and use it to fetch data

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts()
+    //fetch data one time after react component is first rendered. fetch data is asychronous operation,
+    //react eagerly renders component so no matter componentDidMount or componentWillMount the component will be rendered once before data is successfully fetched
+  }
   render() {
     return (
       <div>
@@ -10,4 +17,5 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex
+export default connect(null, { fetchPosts })(PostsIndex)
+//shortcut to hook up action creator function into a component instead of creating a seperate mapDispatchToProps func and use bindActionCreator method from 'redux'
