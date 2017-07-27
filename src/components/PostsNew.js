c592bba7b0eff60a4e97cd6a18382c558ce7aa7b
Minghua Sun <clairesunstudio@gmail.com>
2017-07-27 10:59:44 -0400

    add Field to reduxx form and link component property to a functional component to render jsx markup for the field

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -2,9 +2,26 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
+  renderTitleField(field) {
+    return(
+      <div className="form-group">
+        <label>Title</label>
+        <input className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    )
+  }
   render() {
     return (
-      <div> Post New </div>
+      <form>
+        <Field
+          name="title"
+          component={this.renderTitleField}
+        />
+
+      </form>
     )
   }
 }
@@ -12,4 +29,4 @@ class PostsNew extends Component {
 export default reduxForm({
   form: 'PostsNewForm'  //unique name of this form
 })(PostsNew)
-//reduxForm functions similar to connect helper from react-redux, hook up component to the reducer formReducer
+//reduxForm function similar to connect helper from react-redux, hook up component to the reducer formReducer
