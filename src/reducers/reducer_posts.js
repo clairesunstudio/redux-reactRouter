fc171a5a83a1052a0581bc44b35d263b5d789a53
Minghua Sun <clairesunstudio@gmail.com>
2017-07-27 15:44:38 -0400

    add a case in the reducer to create a new global state with payload data
    keep the data of the previously fetched post and add the newly fetched one to the object collection

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,4 +1,4 @@
-import { FETCH_POSTS } from '../actions'
+import { FETCH_POSTS, FETCH_POST } from '../actions'
 import _ from 'lodash'
 
 export default function(state = {}, action) {
@@ -8,7 +8,15 @@ export default function(state = {}, action) {
       //transform returning array from the api [post1, post2] into objects{{1: post1}, {2: post2}}
       return _.mapKeys(action.payload.data, "id")
       //_.mapKeys method takes in 1st argument: array, 2nd arguemnt: property that will be turn into key of the resulting objects
+    case FETCH_POST:
+      // const post = action.payload.data
+      // const newState = { ...state }
+      // newState[post.id] = post
+      // return NewState
+      //ES5
+      return { ...state, [action.payload.data.id]: action.payload.data } //[] key interpollation to add addtional key: value oject to the collection
       break;
+
     default:
       return state
   }
