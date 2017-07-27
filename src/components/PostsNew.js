ab8feb1c3390f9362f16b3bdea71c9f29200afb0
Minghua Sun <clairesunstudio@gmail.com>
2017-07-27 11:57:24 -0400

    add form validation

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -38,7 +38,28 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  //create an empty errors object
+  const error = {};
+
+  if (!values.title) {
+    error.title = "Enter a title!"
+  }
+  if (!values.categories) {
+    error.categories = "Enter some categories!"
+  }
+  if (!values.content) {
+    error.content = "Enter some content please!"
+  }
+
+  //if errors is empty, form is valid
+  //if errors has *any* properties, redux form assumes form is invalid
+  return error
+}
+
+
 export default reduxForm({
+  validate, //pass validate function as an option to reduxForm
   form: 'PostsNewForm'  //unique name of this form
 })(PostsNew)
 //reduxForm function similar to connect helper from react-redux, hook up component to the reducer formReducer
