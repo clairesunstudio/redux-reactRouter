78b367aacdfd0c52ebdaa628794ba600df010205
Minghua Sun <clairesunstudio@gmail.com>
2017-07-27 09:58:33 -0400

    render PostsNew component in new route and use Switch from react router v4 to render only the component when matching the route exactly

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import promise from 'redux-promise'
 
 import reducers from './reducers';
 import PostsIndex from './components/PostsIndex'
+import PostsNew from './components/PostsNew'
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -14,7 +15,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
+        //Switch looks up the url and matches the first route to render the component, arrange the most specific route at the top
       </div>
     </BrowserRouter>
   </Provider>
