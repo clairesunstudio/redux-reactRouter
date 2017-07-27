79d3e7a237c1b46e9e5387c98366b36956bb9ec4
Minghua Sun <clairesunstudio@gmail.com>
2017-07-27 09:22:18 -0400

    hook up reducer to component for it to access global state as props

--- src/components/PostsIndex.js
+++ src/components/PostsIndex.js
@@ -9,6 +9,7 @@ class PostsIndex extends Component {
     //react eagerly renders component so no matter componentDidMount or componentWillMount the component will be rendered once before data is successfully fetched
   }
   render() {
+    console.log(this.props.posts)
     return (
       <div>
       Posts Index
@@ -17,5 +18,9 @@ class PostsIndex extends Component {
   }
 }
 
-export default connect(null, { fetchPosts })(PostsIndex)
+function mapStateToProps(state) {
+  return { posts: state.posts}
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex)
 //shortcut to hook up action creator function into a component instead of creating a seperate mapDispatchToProps func and use bindActionCreator method from 'redux'
