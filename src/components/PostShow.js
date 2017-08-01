d60cb0d12befcd203af89d9e39a3efde88d74ca2
Minghua Sun <clairesunstudio@gmail.com>
2017-08-01 14:19:16 -0400

    hook up reducer and action creator to fetch and render a single post

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPost } from '../actions'
 
 class PostShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params; //provided by react-router params lists all wildcard values in the url
+    this.props.fetchPost(id)
+
+  }
   render () {
+    const { post } = this.props
+    if (!post) {
+      return <div>Loading</div>
+    }
+
     return (
       <div>
-      post 1
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     )
   }
 }
 
-export default PostShow
+function mapStateToProps({ posts }, ownProps){
+  //mapStateToProps 1st argument is application level state, and 2nd arguement is the equivalent to PostShow.props
+  return { post: posts[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchPost })(PostShow)
