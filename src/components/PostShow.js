cd037d743b1c286c664268db9707c94e12bebabd
Minghua Sun <clairesunstudio@gmail.com>
2017-08-01 16:43:30 -0400

    add deletePost action creator and a button to navigate back to the index triggered in a callback function after sending the delete request

--- src/components/PostShow.js
+++ src/components/PostShow.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { fetchPost } from '../actions'
+import { fetchPost, deletePost } from '../actions'
 
 
 class PostShow extends Component {
@@ -10,6 +10,14 @@ class PostShow extends Component {
     this.props.fetchPost(id)
 
   }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
+      this.props.history.push('/')
+    })//programmatic navigration back to the root as a callback function to ensure the delete request is done
+  }
+
   render () {
     const { post } = this.props
     if (!post) {
@@ -19,6 +27,7 @@ class PostShow extends Component {
     return (
       <div>
         <Link to='/' className="btn btn-primary">Back to Index</Link>
+        <button to='/' className="btn btn-danger pull-xs-right" onClick={this.onDeleteClick.bind(this)}>Delete</button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -32,4 +41,4 @@ function mapStateToProps({ posts }, ownProps){
   return { post: posts[ownProps.match.params.id] }
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostShow)
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostShow)
